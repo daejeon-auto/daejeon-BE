642d2b90353a8ed28d123f4d6063c61194151a26
koldin108902 <alex108902@naver.com>
2022-11-06 05:02:47 +0900

    Initial commit

---
+++ src/main/java/com/pcs/daejeon/DaejeonApplication.java
@@ -0,0 +1,13 @@
+package com.pcs.daejeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaejeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaejeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcs/daejeon/DaejeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcs.daejeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaejeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
