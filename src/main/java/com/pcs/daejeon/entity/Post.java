818431698b518335b08846860eb5d5cb73d2c382
koldin108902 <alex108902@naver.com>
2022-11-06 22:22:23 +0900

    update: post liked setter, post constructor

--- src/main/java/com/pcs/daejeon/entity/Post.java
+++ src/main/java/com/pcs/daejeon/entity/Post.java
@@ -16,12 +16,22 @@ public class Post extends BasicTime {
     @Enumerated(EnumType.STRING)
     private PostType postType;
 
-    @Size(max = 100, min = 10)
+    @Size(max = 100, min = 5)
     private String description;
 
     private int liked;
 
+    public Post() {}
+
     public void setPostType(PostType postType) {
         this.postType = postType;
+    }
+
+    public void addLiked() {
+        this.liked++;
+    }
+
+    public Post(String description) {
+        this.description = description;
     }
 }
