04fa336305ca6e90dbf09771f3abe96ac6982376
koldin108902 <alex108902@naver.com>
2022-11-09 13:43:13 +0900

    update: postDto add liked value

--- src/main/java/com/pcs/daejeon/dto/PostDto.java
+++ src/main/java/com/pcs/daejeon/dto/PostDto.java
@@ -1,21 +1,17 @@
 package com.pcs.daejeon.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Setter
-@Getter
+@AllArgsConstructor
+@Setter @Getter
 public class PostDto {
 
     private Long postId;
     private String description;
     private LocalDateTime created;
-
-    public PostDto(Long postId, String description, LocalDateTime created) {
-        this.postId = postId;
-        this.description = description;
-        this.created = created;
-    }
+    private int liked;
 }
