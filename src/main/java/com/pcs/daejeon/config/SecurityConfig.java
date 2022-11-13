6bab3ca61f17425043708098836a9bd8aeb95591
koldin108902 <alex108902@naver.com>
2022-11-13 12:54:29 +0900

    update: login controller

--- src/main/java/com/pcs/daejeon/config/SecurityConfig.java
+++ src/main/java/com/pcs/daejeon/config/SecurityConfig.java
@@ -22,8 +22,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
+                .antMatchers("/post/**").authenticated()
                 .antMatchers("/admin/**").access("hasRole('TIER1') or hasRole('TIER2') or hasRole('TIER3')") // 해당 권한을 가진 사람만 접근 가능
-                .anyRequest().permitAll(); // 다른 주소는 모두 허용
+                .anyRequest().permitAll() // 다른 주소는 모두 허용
+                .and()
+                .formLogin()
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/");
     }
 
     @Bean
