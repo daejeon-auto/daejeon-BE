4f5801e9eae1c63e7df27a87ffa9a1ce5f448bce
koldin108902 <alex108902@naver.com>
2022-11-18 16:05:19 +0900

    update: can access unsigned in user

--- src/main/java/com/pcs/daejeon/repository/customImpl/PostRepositoryImpl.java
+++ src/main/java/com/pcs/daejeon/repository/customImpl/PostRepositoryImpl.java
@@ -5,14 +5,15 @@ import com.pcs.daejeon.entity.type.PostType;
 import com.pcs.daejeon.repository.custom.PostRepositoryCustom;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
-
-import static com.pcs.daejeon.entity.QLike.*;
-import static com.pcs.daejeon.entity.QPost.*;
+import static com.pcs.daejeon.entity.QLike.like;
+import static com.pcs.daejeon.entity.QPost.post;
 
 @RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepositoryCustom {
@@ -21,18 +22,29 @@ public class PostRepositoryImpl implements PostRepositoryCustom {
 
     @Override
     public QueryResults<Tuple> pagingPost(Pageable page) {
-        PrincipalDetails member = (PrincipalDetails) SecurityContextHolder
-            .getContext()
-            .getAuthentication()
-            .getPrincipal();
+        PrincipalDetails member = null;
+        if (SecurityContextHolder.getContext().getAuthentication() != null &&
+                SecurityContextHolder.getContext().getAuthentication().getPrincipal()!= null &&
+                SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof UserDetails) {
 
-        QueryResults<Tuple> result = query
+            member = (PrincipalDetails) SecurityContextHolder
+                    .getContext()
+                    .getAuthentication()
+                    .getPrincipal();
+        }
+
+        JPAQuery<Tuple> tupleJPAQuery = query
                 .select(post, like)
                 .from(post)
                 .where(post.postType.eq(PostType.ACCEPTED))
-                .orderBy(post.id.desc())
+                .orderBy(post.id.desc());
+        if (member != null) {
+            tupleJPAQuery
                 .leftJoin(post.like, like)
-                .on(like.post.id.eq(post.id), like.likedBy.id.eq(member.getMember().getId()))
+                .on(like.post.id.eq(post.id), like.likedBy.id.eq(member.getMember().getId()));
+        }
+
+        QueryResults<Tuple> result = tupleJPAQuery
                 .offset(page.getOffset())
                 .limit(20)
                 .fetchResults();
