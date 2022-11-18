2e407c0a2908b5f248b14a7ec1848ff4d78e1dcc
koldin108902 <alex108902@naver.com>
2022-11-19 03:00:49 +0900

    update: CORS - can access localhost:3000

--- src/main/java/com/pcs/daejeon/config/WebMvcConfig.java
+++ src/main/java/com/pcs/daejeon/config/WebMvcConfig.java
@@ -1,14 +1,22 @@
 package com.pcs.daejeon.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         WebMvcConfigurer.super.configureViewResolvers(registry);
+    }
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
     }
 }
