db180c58727f2fd3c080d4681a66d74c3a1bd90a
koldin108902 <alex108902@naver.com>
2022-11-23 09:26:01 +0900

    fixed: auditing setting member null

--- src/main/java/com/pcs/daejeon/repository/customImpl/AuditorAwareImpl.java
+++ src/main/java/com/pcs/daejeon/repository/customImpl/AuditorAwareImpl.java
@@ -4,6 +4,7 @@ import com.pcs.daejeon.config.auth.PrincipalDetails;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
@@ -12,10 +13,18 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     @NonNull
     public Optional<String> getCurrentAuditor() {
-        PrincipalDetails member = (PrincipalDetails) SecurityContextHolder
-                .getContext()
-                .getAuthentication()
-                .getPrincipal();
+        PrincipalDetails member = null;
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+            Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            if (principal != null && principal instanceof UserDetails) {
+                member = (PrincipalDetails) principal;
+            }
+        }
+
+        if (member == null) {
+            return Optional.empty();
+        }
+
         return Optional.of(member.getMember().getId().toString());
     }
 }
