d62cd760f225196bb92172d1122ff50605fc7350
koldin108902 <alex108902@naver.com>
2022-12-07 14:14:50 +0900

    update: test용 DB설정 분활

--- src/main/java/com/pcs/daejeon/repository/customImpl/AuditorAwareImpl.java
+++ src/main/java/com/pcs/daejeon/repository/customImpl/AuditorAwareImpl.java
@@ -1,9 +1,11 @@
 package com.pcs.daejeon.repository.customImpl;
 
 import com.pcs.daejeon.config.auth.PrincipalDetails;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.lang.NonNull;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
@@ -14,10 +16,16 @@ public class AuditorAwareImpl implements AuditorAware<String> {
     @NonNull
     public Optional<String> getCurrentAuditor() {
         PrincipalDetails member = null;
+        String testMember = null;
         if (SecurityContextHolder.getContext().getAuthentication() != null) {
             Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-            if (principal != null && principal instanceof UserDetails) {
+            if (principal instanceof PrincipalDetails) {
                 member = (PrincipalDetails) principal;
+            } else if (principal instanceof User) {
+                testMember = ((User) principal).getUsername();
+                return Optional.of(testMember);
+            } else {
+                return Optional.of(principal.toString());
             }
         }
 
