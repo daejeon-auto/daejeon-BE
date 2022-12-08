55094716f0a1be0e36546a25b0a4b1579b1e2c56
koldin108902 <alex108902@naver.com>
2022-12-09 04:40:56 +0900

    add: rejected post list test

--- src/test/java/com/pcs/daejeon/repository/customImpl/PostRepositoryImplTest.java
+++ src/test/java/com/pcs/daejeon/repository/customImpl/PostRepositoryImplTest.java
@@ -3,13 +3,13 @@ package com.pcs.daejeon.repository.customImpl;
 import com.pcs.daejeon.WithMockCustomUser;
 import com.pcs.daejeon.entity.Post;
 import com.pcs.daejeon.entity.QPost;
+import com.pcs.daejeon.entity.type.PostType;
 import com.pcs.daejeon.repository.PostRepository;
 import com.querydsl.core.Tuple;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
@@ -17,13 +17,16 @@ import org.springframework.test.context.ActiveProfiles;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
 @Rollback
+@WithMockCustomUser
 class PostRepositoryImplTest {
 
     @Autowired
@@ -33,7 +36,6 @@ class PostRepositoryImplTest {
     EntityManager em;
 
     @BeforeEach
-    @WithMockCustomUser
     public void initData() {
         for (int i = 0; i < 100; i++) {
             postRepository.save(new Post("test value " + i));
@@ -41,7 +43,6 @@ class PostRepositoryImplTest {
     }
 
     @Test
-    @WithMockCustomUser
     public void unloggedInPagingPost() {
         PageRequest page = PageRequest.of(0, 10);
         Page<Tuple> post = postRepository.pagingPost(page);
@@ -51,6 +52,28 @@ class PostRepositoryImplTest {
 
         for (int i = 0; i < post.getContent().size(); i++) {
             assertThat(post.getContent().get(i).get(QPost.post).getDescription()).isEqualTo("test value " + (99-i));
+        }
+    }
+
+    @Test
+    public void 신고된_글_리스트() {
+        PageRequest pageable = PageRequest.of(0, 10);
+
+        // == reject post 생성 ==
+        Page<Tuple> tuples = postRepository.pagingPost(pageable);
+
+        for (Tuple tuple : tuples.getContent()) {
+            Objects.requireNonNull(tuple.get(QPost.post)).setPostType(PostType.REJECTED);
+        }
+        // == reject post 생성 ==
+
+        Page<Post> posts = postRepository.pagingRejectPost(pageable, null, null);
+
+        List<Post> content = posts.getContent();
+
+        for (int i = 0; i < content.size(); i++) {
+            assertThat(content.get(i).getDescription()).isEqualTo("test value " + (99-i));
+            assertThat(content.get(i).getPostType()).isEqualTo(PostType.REJECTED);
         }
     }
 
