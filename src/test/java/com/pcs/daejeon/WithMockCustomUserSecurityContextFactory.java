e96ce3a58c5d2be2d58ae2a06b8091f91488b70b
koldin108902 <alex108902@naver.com>
2022-12-09 04:40:35 +0900

    fixed: test를 위한 가상 계정 - PrincipalDetails class를 활용해 오류가 나지 않을 것임(실제 계정과 동일)

--- src/test/java/com/pcs/daejeon/WithMockCustomUserSecurityContextFactory.java
+++ src/test/java/com/pcs/daejeon/WithMockCustomUserSecurityContextFactory.java
@@ -1,22 +1,46 @@
 package com.pcs.daejeon;
 
+import com.pcs.daejeon.config.auth.PrincipalDetails;
+import com.pcs.daejeon.entity.Member;
+import com.pcs.daejeon.entity.type.AuthType;
+import com.pcs.daejeon.entity.type.RoleTier;
+import com.pcs.daejeon.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Component
 public class WithMockCustomUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Override
     public SecurityContext createSecurityContext(WithMockCustomUser annotation) {
 
         final SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
 
+        Member member = new Member(
+                "test",
+                "050323",
+                "01027729778",
+                "20201",
+                "password",
+                "loginId",
+                AuthType.DIRECT
+        );
+        member.setRole(RoleTier.ROLE_TIER2);
+        memberRepository.save(member);
+
         final UsernamePasswordAuthenticationToken authenticationToken
-                = new UsernamePasswordAuthenticationToken(annotation.loginId(),
+                = new UsernamePasswordAuthenticationToken(new PrincipalDetails(
+                member),
                 "password",
                 Arrays.asList(new SimpleGrantedAuthority(annotation.role())));
 
