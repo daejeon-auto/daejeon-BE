b632c5beedcb32a0cafb4b740621af630ccc97d4
koldin108902 <alex108902@naver.com>
2023-01-22 13:40:41 +0900

    update: 검증 추가

--- src/test/java/com/pcs/daejeon/controller/MemberControllerTest.java
+++ src/test/java/com/pcs/daejeon/controller/MemberControllerTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pcs.daejeon.WithMockCustomUser;
 import com.pcs.daejeon.dto.member.SignUpDto;
 import com.pcs.daejeon.entity.type.AuthType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,8 +14,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -34,6 +39,7 @@ class MemberControllerTest {
     ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
+    @DisplayName("회원가입 성공")
     void signUp() throws Exception {
         SignUpDto user = new SignUpDto(
                 "test1",
@@ -48,11 +54,18 @@ class MemberControllerTest {
                 "인스타아이디",
                 "인스타비밀번호"
         );
-        mvc.perform(MockMvcRequestBuilders
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders
                         .post("/sign-up")
                         .content(objectMapper.writeValueAsString(user))
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        // String result to object
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = mapper.readValue(mvcResult.getResponse().getContentAsString(), Map.class);
+
+        Assertions.assertThat(map.get("hasError")).isEqualTo(false);
     }
 
     @Test
