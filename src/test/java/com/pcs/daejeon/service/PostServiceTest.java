0e52d46ec3c2fe6a05c1953a25f9633be67c2d52
koldin108902 <alex108902@naver.com>
2023-01-24 10:40:18 +0900

    delete: mvc

--- src/test/java/com/pcs/daejeon/service/PostServiceTest.java
+++ src/test/java/com/pcs/daejeon/service/PostServiceTest.java
@@ -18,7 +18,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -45,9 +44,6 @@ class PostServiceTest {
 
     @Autowired
     MemberRepository memberRepository;
-
-    @Autowired
-    MockMvc mvc;
 
     @Autowired
     EntityManager em;
