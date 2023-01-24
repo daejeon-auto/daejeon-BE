8fad66686e8096176de83184423c3568f6b6fc12
koldin108902 <alex108902@naver.com>
2023-01-25 04:15:25 +0900

    fixed: activeProfile을 test로 설정

--- src/test/java/com/pcs/daejeon/service/PostServiceTest.java
+++ src/test/java/com/pcs/daejeon/service/PostServiceTest.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
@@ -31,6 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 @Rollback
 @WithMockCustomUser
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class PostServiceTest {
 
