28f3c4d42d9665e06f0129cb85ffca9956bfa763
koldin108902 <alex108902@naver.com>
2023-02-05 20:48:50 +0900

    update: 같은 학교일 시 작성 가능

--- src/main/java/com/pcs/daejeon/repository/customImpl/ReportRepositoryImpl.java
+++ src/main/java/com/pcs/daejeon/repository/customImpl/ReportRepositoryImpl.java
@@ -1,14 +1,13 @@
 package com.pcs.daejeon.repository.customImpl;
 
 import com.pcs.daejeon.common.Util;
-import com.pcs.daejeon.entity.Member;
-import com.pcs.daejeon.entity.QMember;
-import com.pcs.daejeon.entity.QPost;
-import com.pcs.daejeon.entity.Report;
+import com.pcs.daejeon.entity.*;
 import com.pcs.daejeon.repository.custom.ReportRepositoryCustom;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Objects;
 
 import static com.pcs.daejeon.entity.QReport.report;
 
@@ -16,7 +15,7 @@ import static com.pcs.daejeon.entity.QReport.report;
 @RequiredArgsConstructor
 public class ReportRepositoryImpl implements ReportRepositoryCustom {
 
-    private final JPAQueryFactory jpaQueryFactory;
+    private final JPAQueryFactory query;
     private final Util util;
 
     @Override
@@ -26,12 +25,22 @@ public class ReportRepositoryImpl implements ReportRepositoryCustom {
         QMember reportedBy = new QMember("reportedBy");
         QPost reportedPost = new QPost("reportedPost");
 
-        Report result = jpaQueryFactory
+        Report result = query
                 .selectFrom(report)
                 .innerJoin(report.reportedBy, reportedBy)
                 .innerJoin(report.reportedPost, reportedPost)
                 .where(reportedBy.id.eq(member.getId()), reportedPost.id.eq(postId))
                 .fetchOne();
-        return result == null;
+
+        // 해당 학교와 같은 학교인지 확인
+        School school = query
+                .selectFrom(QPost.post.school)
+                .where(QPost.post.id.eq(postId))
+                .fetchOne();
+
+        return result == null &&
+                Objects.equals(
+                        util.getLoginMember().getSchool().getId(),
+                        Objects.requireNonNull(school).getId());
     }
 }
