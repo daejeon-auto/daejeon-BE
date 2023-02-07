96469c2423a69f042ba1d081a3b0ffc3d5024049
koldin108902 <alex108902@naver.com>
2023-02-08 05:24:35 +0900

    fixed: dto에 allArgumentConstructor가 없었음

--- src/main/java/com/pcs/daejeon/dto/member/SignUpAdminDto.java
+++ src/main/java/com/pcs/daejeon/dto/member/SignUpAdminDto.java
@@ -1,10 +1,12 @@
 package com.pcs.daejeon.dto.member;
 
 import com.pcs.daejeon.dto.school.SchoolRegistDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class SignUpAdminDto {
 
--- src/main/java/com/pcs/daejeon/dto/school/SchoolRegistDto.java
+++ src/main/java/com/pcs/daejeon/dto/school/SchoolRegistDto.java
@@ -1,5 +1,6 @@
 package com.pcs.daejeon.dto.school;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class SchoolRegistDto {
 
