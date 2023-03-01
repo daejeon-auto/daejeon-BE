0670d704dc366b5cf548db0c72ce6382b5c2592c
koldin108902 <alex108902@naver.com>
2023-03-02 04:27:04 +0900

    fixed: SignUpAdminDto에 필드값 not null

--- src/main/java/com/pcs/daejeon/dto/member/SignUpAdminDto.java
+++ src/main/java/com/pcs/daejeon/dto/member/SignUpAdminDto.java
@@ -5,11 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class SignUpAdminDto {
 
+    @NotNull
     private SignUpDto member;
+    @NotNull
     private SchoolRegistDto school;
 }
