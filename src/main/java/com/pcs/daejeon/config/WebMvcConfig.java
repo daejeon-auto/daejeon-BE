041092cc8c6c148a9c7413c4e34037bb3ac544d5
koldin108902 <alex108902@naver.com>
2023-03-07 22:15:23 +0900

    update: repl 사이트 도메인 추가

--- src/main/java/com/pcs/daejeon/config/WebMvcConfig.java
+++ src/main/java/com/pcs/daejeon/config/WebMvcConfig.java
@@ -17,7 +17,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8000", "https://localhost", "https://daejeon.vercel.app", "https://daej-inab.netlify.app")
+                .allowedOrigins("http://localhost:8000", "https://localhost",
+                        "https://daejeon.vercel.app", "https://daej-inab.netlify.app", "https://daejeon.inab-devs.repl.co")
                 .allowCredentials(true)
                 .exposedHeaders("isLogin")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","));
