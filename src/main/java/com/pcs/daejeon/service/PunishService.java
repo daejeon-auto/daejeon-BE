540e2ba969dc9353b3efff4ad9a1acdfc15f1ff3
koldin108902 <alex108902@naver.com>
2023-04-16 23:24:23 +0900

    add: 정지 기록 수정

--- src/main/java/com/pcs/daejeon/service/PunishService.java
+++ src/main/java/com/pcs/daejeon/service/PunishService.java
@@ -1,6 +1,7 @@
 package com.pcs.daejeon.service;
 
 import com.pcs.daejeon.dto.punish.PunishAddDto;
+import com.pcs.daejeon.dto.punish.PunishUpdateDto;
 import com.pcs.daejeon.entity.Member;
 import com.pcs.daejeon.entity.Punish;
 import com.pcs.daejeon.repository.MemberRepository;
@@ -10,6 +11,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,6 +37,26 @@ public class PunishService {
                 punishAddDto.getRating(),
                 isValidMember.get());
         punishRepository.save(punish);
+    }
+
+    public Punish updatePunish(PunishUpdateDto punishUpdateDto) {
+        Optional<Punish> findPunish = punishRepository.findById(punishUpdateDto.getPunishId());
+
+        if (findPunish.isEmpty()) throw new IllegalStateException("punish not found");
+        if (findPunish.get().getExpired_date().isAfter(LocalDateTime.now()))
+            throw new IllegalStateException("it's already expired");
+
+        Punish punish = findPunish.get();
+
+        punish.setRating(punishUpdateDto.getRating());
+        LocalDateTime newPunishExpiredDate = punish.getExpired_date().plusDays(punishUpdateDto.getUpdateDate());
+
+        // 업데이트하려는 시간이 너무 늦으면 에러 발생
+        if (newPunishExpiredDate.isAfter(LocalDateTime.now())) throw new IllegalStateException("too low expire date");
+
+        punish.setExpired_date(newPunishExpiredDate);
+
+        return punish;
     }
 
     public List<Punish> getPunish(Member member) {
