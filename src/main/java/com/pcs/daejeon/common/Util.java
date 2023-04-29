da0914d9b66f26f0370140328d1416f5ce1d7086
koldin108902 <alex108902@naver.com>
2023-04-30 08:54:43 +0900

    fixed: AES key byte size

--- src/main/java/com/pcs/daejeon/common/Util.java
+++ src/main/java/com/pcs/daejeon/common/Util.java
@@ -11,8 +11,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 import java.util.Optional;
 
@@ -50,11 +54,11 @@ public class Util {
     }
 
     private static final String ALGORITHM = "AES";
-    private static final byte[] keyValue = "inab@anonpost.secretkey.forencrypt.instagramInfo".getBytes();
+    private static final byte[] keyValue = "inab@anonpost.secretkey".getBytes();
 
     public static String encrypt(String valueToEnc, String salt) throws Exception {
         String saltedVal = valueToEnc + salt;
-        Key key = generateKey();
+        Key key = generateKey(salt);
         Cipher c = Cipher.getInstance(ALGORITHM);
         c.init(Cipher.ENCRYPT_MODE, key);
         byte[] encValue = c.doFinal(saltedVal.getBytes());
@@ -62,7 +66,7 @@ public class Util {
     }
 
     public static String decrypt(String encryptedValue, String salt) throws Exception {
-        Key key = generateKey();
+        Key key = generateKey(salt);
         Cipher c = Cipher.getInstance(ALGORITHM);
         c.init(Cipher.DECRYPT_MODE, key);
         byte[] decordedValue = Base64.getDecoder().decode(encryptedValue);
@@ -71,9 +75,19 @@ public class Util {
         return decStr.substring(0, decStr.length() - salt.length());
     }
 
-    private static Key generateKey() throws Exception {
-        Key key = new SecretKeySpec(keyValue, ALGORITHM);
+    private static Key generateKey(String salt) throws Exception {
+        Key key = new SecretKeySpec(hashPassword(salt.toCharArray(), keyValue, 1000, 256), ALGORITHM);
         return key;
+    }
+
+    private static byte[] hashPassword(char[] password, byte[] salt, int iterations, int keyLength) {
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, keyLength);
+            return skf.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
